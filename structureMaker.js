94893a7398d503e5f47089c3693939cc60872bb1
Vterebenin <niceserver11@mail.ru>
2019-09-08 21:18:31 +0300

    initial

---
+++ structureMaker.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const fs = require('fs');
+const glob = require('glob')
+const { readdirSync } = require('fs')
+
+const getDirectories = source =>
+    readdirSync(source, { withFileTypes: true })
+        .filter(webDirent => webDirent.isDirectory())
+        .map(webDirent => webDirent.name)
+const webDirectories = getDirectories("./skewer/build/Page/")
+
+webDirectories.forEach(moduleFolder => {
+    function createDevDirAndCopyFilesFor(asset) {
+        
+        const webDir = `./skewer/build/Page/${moduleFolder}/web/`;
+        const assetDir = `./skewer/build/Page/${moduleFolder}/web/${asset}/`;
+        const devDir = `./skewer/build/Page/${moduleFolder}/web/${asset}/dev/`;
+    
+    
+        // * сначала прохоим по файлу и создаем необходимые директории, если они не существуют
+        if (!fs.existsSync(webDir)) {
+            fs.mkdirSync(webDir);
+            console.log("web folder created for ", moduleFolder);
+        }
+        if (!fs.existsSync(assetDir)) {
+            fs.mkdirSync(assetDir);
+            console.log(`${asset} folder created for `, moduleFolder);
+        }
+        if (!fs.existsSync(devDir)) {
+            fs.mkdirSync(devDir);
+            console.log("dev folder created for ", moduleFolder);
+        }
+    
+        // * затем копируем оригиналы файлов в папку dev, чтобы не менять везде зависимости
+        glob(`*.${asset}`, { cwd: assetDir, nodir: true }, function (er, files) {
+            files.forEach(entryName => {
+                copyFromName = `${assetDir}${entryName}`
+                copyToName = `${devDir}${entryName}`
+    
+                console.log("____________________________________________________");
+                console.log("мы копируем это", copyFromName);
+                console.log("cюда", copyToName);
+                // entryObject[name] = looker
+                fs.copyFile(copyFromName, copyToName, (err) => {
+                    if (err) throw err;
+                    console.log('copied!');
+                });
+            })
+        })
+    }
+
+    createDevDirAndCopyFilesFor('js');
+    createDevDirAndCopyFilesFor('css');
+});
---
+++ webpack.config.js
@@ -0,0 +1,116 @@
+
+const path = require('path')
+const glob = require('glob')
+const webpack = require('webpack');
+const { readdirSync } = require('fs')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+// const StyleLintPlugin = require('stylelint-webpack-plugin');
+// const cssNano = require('cssnano')
+
+const getDirectories = source =>
+  readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name)
+
+const moduleDirectories = getDirectories("./skewer/build/Page/")
+let entryObject = {}
+moduleDirectories.forEach(moduleFolder => {
+  function createEntryObjectFor(asset) {
+    const assetDir = `./skewer/build/Page/${moduleFolder}/web/${asset}/`;
+    const devDir = `./skewer/build/Page/${moduleFolder}/web/${asset}/dev/`;
+    const files = glob.sync(`*.${asset}`, { cwd: devDir, nodir: true })
+    if (files.length) {
+      files.forEach(entryName => {
+        // для сss нужно специальный массив, где name пишется без своего файлового расширения
+        assetName = asset === "css"
+          ? path.basename(entryName, ".css")
+          : entryName
+        // путь куда файл будет компилироваться
+        name = `${assetDir}${assetName}`
+        // путь откуда файл будет компилироваться
+        looker = `${devDir}${entryName}`
+        entryObject[name] = looker
+      })
+    }
+  }
+  createEntryObjectFor('js')
+  createEntryObjectFor('css')
+})
+
+module.exports = {
+  
+  mode: 'development',
+  
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        sourceMap: true
+      })
+    ],
+  },
+  entry: entryObject,
+  // entry: ['./allJsFilesOfFolder.js'],
+  output: {
+    path: path.resolve(__dirname, './'),
+    filename: '[name]',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          // style-loader
+          { loader: MiniCssExtractPlugin.loader },
+          // { loader: 'handlebars-loader' },
+          // { loader: 'extract-loader' },
+          // css-loader
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+              modules: false,
+              // minimize: true, //|| {/* CSSNano Options */ }
+              // importLoaders: 1
+            }
+          },
+          // {
+          //   loader: 'postcss-loader',
+          //   options: {
+          //     config: {
+          //       path: './'
+          //     }
+          //   }
+          // },
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+      },
+    ]
+  },
+  plugins: [
+    // new CleanWebpackPlugin(),
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      ignoreOrder: true,
+    }),
+
+    // new cssNano({
+    //   preset: 'default',
+    // }),
+
+    require('autoprefixer')
+    // ? раскомментировать, когда будет мертв диз. режим
+    // new StyleLintPlugin({
+    //   files: './skewer/build/Page/*/web/css/dev/*.css',
+    //   configFile: './.stylelintrc'
+    // }),
+  ],
+  stats: 'errors-warnings'
+};
